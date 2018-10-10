d1c582ac49c61df167b2f69da6e37e09e2b709bd
Vida Pao <beneathasteroids2018@gmail.com>
2018-10-11 02:06:44 +0800

    Initial commit

---
+++ Assets/earth.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class earth : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Transform transform = GetComponent<Transform>();
+        Vector3 angle = new Vector3(0, 1, 0);
+        transform.Rotate(angle);
+    }
+}
---
+++ Assets/moon.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class moon : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Transform transform = GetComponent<Transform>();
+        Vector3 angle = new Vector3(0, 0.4f, 0);
+        transform.Rotate(angle);
+    }
+}
